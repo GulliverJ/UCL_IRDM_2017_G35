699a25efaf744be413232afe868ab355453178eb
Gulliver Johnson <gulliverjohnson@gmail.com>
2017-04-14 19:19:35 +0100

    Tidying up

--- web/main.py
+++ web/main.py
@@ -3,24 +3,25 @@ from flask import request
 from flask import render_template
 
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
+def index():
 	return render_template("index.html")
 
-@app.route("/results", methods=['GET', 'POST'])
+@app.route("/results", methods=['GET'])
 def results():
-    results = searchForResults(request.args['query'])
+    results = getRankedResults(request.args['query'])
     return render_template("index.html", results=results)
 
 
-def searchForResults(query):
+### TEMPORARY METHOD; SHOULD CALL APPROPRIATE RANKING ALGO
+def getRankedResults(query):
     res = [
         { "title": "Result 1", "href": "http://www.cs.ucl.ac.uk", "blurb": "CS main page"},
         { "title": "Result 2", "href": "http://www.cs.ucl.ac.uk/prospective_students", "blurb": "Prospective Students"},
         { "title": "Result 3", "href": "http://www.cs.ucl.ac.uk/careers", "blurb": "Careers page"},
     ]
     return res
-
 
 if __name__ == "__main__":
 	app.run()
